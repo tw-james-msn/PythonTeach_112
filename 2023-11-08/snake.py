e1c9aaa5a1378266e320b34f848bb5a9537b960a
james-vscode <151116627+tw-james-msn@users.noreply.github.com>
2023-11-17 15:55:27 +0800

    Update 2023-11-08/snake.py

---
+++ 2023-11-08/snake.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+pygame.init() # 初始化pygame的模組(一開始固定要寫的)
+
+WIDTH = 160    # 遊戲畫面寬
+HEIGHT = 100   # 遊戲畫面高
+SCALE = 5
+
+screen = pygame.display.set_mode( ( WIDTH*SCALE, HEIGHT*SCALE ) )  # 設定遊戲視窗的尺寸(size)
+
+pygame.display.set_caption('我的貪食蛇遊戲') # 遊戲視窗的抬頭
+
+
+running = True  # 控制遊戲迴圈的變數
+
+# 遊戲迴圈 --Begin--------
+while running:
+
+  # 一一取得所有曾發生的event(事件)來檢查
+  for event in pygame.event.get():
+
+    if event.type == pygame.QUIT: # 按視窗右上角x的事件
+      running = False   # 離開遊戲迴圈 while running
+      break             # 離開 for event 迴圈
+
+    # 檢查其它事件
+    #  .
+    #  .
+    #  ..
+
+  #--事件檢查-------------
+
+
+# 遊戲迴圈 --End--
+
+pygame.quit() # 結束 pygame模組(結束固定要寫的)

d5efbebfbdfa3ab623f7dfb23987bc5b8788b931
james-vscode <151116627+tw-james-msn@users.noreply.github.com>
2023-12-21 09:54:30 +0800

    Update war/main_2023-12-20.py

--- war/main_2023-12-20.py
+++ war/main_2023-12-20.py
@@ -18,56 +18,60 @@ clock = pygame.time.Clock()     # 控制遊戲速度用
 FPS = 60                        # 每秒幾楨畫面
 
 # 載入圖片
-background_img = pygame.image.load("2023-12-20/img/background.png").convert()
-player_img = pygame.image.load("2023-12-20/img/player.png").convert()
+background_img = pygame.image.load("img/background.png").convert()
+player_img = pygame.image.load("img/player.png").convert()
 
 # 載入音效 & 音樂
 pygame.mixer.init()
-pygame.mixer.music.load("2023-12-20/sound/background.ogg")
+pygame.mixer.music.load("sound/background.ogg")
 pygame.mixer.music.set_volume(0.4)
 
+#############
+## 物件類別 ##
+############
 
-# 物件類別
-## Player
+## 類別--Player
 class Player(pygame.sprite.Sprite):
   def __init__(self):
     pygame.sprite.Sprite.__init__(self)   # 先執行父親的初始化，再執行下面我自己的初始化
     self.img = pygame.transform.scale(player_img, (50, 38))
     
-
-
-# 副程式
+###########
+## 副程式 ##
+###########
+    
+## 副程式--結束程式
 def theEnd():
   pygame.quit()
   sys.exit()
 
-## 副程式--在遊戲畫面畫出文字
+## 副程式--在遊戲畫面畫出文字(需提供參數：要顯示畫面、文字內容、x座標、y座標)
 def draw_text(surf, text, size, x, y):
-    font = pygame.font.SysFont('MicrosoftJhenghei,華康超圓體', size)  # 建立字型物件供顯示文字訊息時用
-    text_surface = font.render(text, True, COLOR_WHITE)
-    text_rect = text_surface.get_rect()
-    text_rect.centerx = x
-    text_rect.top = y
-    surf.blit(text_surface, text_rect)
+    font = pygame.font.SysFont('MicrosoftJhenghei,pingfang', size)   # 建立字型物件供顯示文字訊息時用
+    text_surface = font.render(text, True, COLOR_WHITE)     # 用所選字型渲染白色非鋸齒的text文字
+    text_rect = text_surface.get_rect()       # 取得渲染後的文字圖形(可以放入遊戲畫面的)
+    text_rect.centerx = x                     # 設定文字水平軸的顯示位置(中心位置在傳入的x座標)
+    text_rect.top = y                         # 設定文字水平軸的顯示位置(上方位置在傳入的y座標)
+    surf.blit(text_surface, text_rect)        # 在傳入的畫面上畫上去(尚未更新到真正的螢幕上)
 
 ## 副程式--秀出遊戲初始說明畫面直到user按任意鍵開始玩遊戲
 def draw_init():
-  screen.blit(background_img, (0,0))
+  screen.blit(background_img, (0,0))    # 用blit()貼上背景圖片
   draw_text(screen, '太空生存戰!', 64, WIDTH/2, HEIGHT/4)
   draw_text(screen, '← →移動飛船 空白鍵發射子彈~', 22, WIDTH/2, HEIGHT/2)
   draw_text(screen, '按任意鍵開始遊戲!', 18, WIDTH/2, HEIGHT*3/4)
+  pygame.display.update()   # 將畫出的文字真正的更新到顯示器上，使用者可以看到
 
-  pygame.display.update()
-
-  while True:
+  # 等待使用選擇下一步要做什麼的迴圈
+  while True:     
     clock.tick(FPS)
-    for e in pygame.event.get():  # 取得輸入
-      if e.type == pygame.QUIT:   # 若使用者按 X 關閉遊戲那就 the end
+    for e in pygame.event.get():      # 迭代取得所有發生的events
+      if e.type == pygame.QUIT:       # 若使用者按 X 關閉遊戲那就 the end
         theEnd()
-      elif e.type == pygame.KEYDOWN:  # 若使用者隨便按了一個鍵那就
-        return                        # 離開等待使用者按鍵的迴圈，返回主程式
+      if e.type == pygame.KEYDOWN:    # 否則若使用者隨便按了一個鍵那就
+        return                        # 離開本副程式，返回主程式
 
-## 副程式--產生新的 rock
+## 副程式--產生新的rock
 def new_rock():
   pass
 
